a09b7121dc69962ad8c4679a246749fbf1b45cb2
Nate Allen <nateonmission@gmail.com>
2022-02-01 13:03:16 -0500

    added searchPeople service

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-people',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
+  person: any = {firstName: ""};
+  field: string = "";
+  search: string = "";
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+
+
+
+  searchPeople(field: String, search: string){
+    // if(field==="email"){
+    //   this.person = this.http.get(`https://church-mgr-proj.herokuapp.com/api/v1/people/email/'${search}'`);  private http: HttpClient
+    //   console.log(this.person);
+    // } else {
+    //   this.person = this.http.get(`https://church-mgr-proj.herokuapp.com/api/v1/people/phoneNumber/'${search}'`);
+    //   console.log(this.person);  
+    // }
+    this.person.firstName = "Nate";
+    console.log(this.person);
+  }
+
+
+
+
 
 }
