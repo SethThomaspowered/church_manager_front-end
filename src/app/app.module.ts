56206d0bd1789f07815100eb953aef32858b5431
Nate Allen <nateonmission@gmail.com>
2022-02-01 11:26:08 -0500

    created components and structured basing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PeopleComponent } from './people/people.component';
+import { GroupsComponent } from './groups/groups.component';
+import { MoneyComponent } from './money/money.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PeopleComponent,
+    GroupsComponent,
+    MoneyComponent
   ],
   imports: [
     BrowserModule,
