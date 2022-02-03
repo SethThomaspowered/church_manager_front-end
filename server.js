0eb8889d18a8c48a79f6c2dc041c9de7d7dd7884
Nate Allen <nateonmission@gmail.com>
2022-02-03 17:21:32 -0500

    updated server.js for fe deployment again again again

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/church-mgr-fe'));
+app.use(express.static(__dirname + '/'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'church-mgr-fe', 'index.html'))
+res.sendFile(path.join(__dirname, 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
