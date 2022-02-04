ae71d6d68eedb081d89dd1b94ec492c62d62cb7e
Nate Allen <nateonmission@gmail.com>
2022-02-03 22:27:44 -0500

    began landing page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  church = {
+    name: "Hometown Church",
+    address1: "1303 S Shelby St",
+    address2: "",
+    city: "Louisville",
+    state: "Kentucky",
+    zip: "40217",
+    areaCode: "502",
+    exchange: "635",
+    phone: "7053",
+    email: "hometownchurch@domain.com",
+    lat: 38.2339879,
+    lon: -85.7421174
+
+};
+user = {
+
+  first: "Jerome",
+  last: "Allen",
+  accessLevel: "Developer",
+  loginStamps: ["2022.01.31", "2022.02.01", "2022.02.02", "2022.02.03", "2022.02.04"]
+};
+
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.user);
   }
+
+  
 
 }
--- src/app/userDetails.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-
-    first: string = "Jerome";
-    last: string = "Allen";
-    accessLevel: string = "Developer";
-    loginStamps: any = ["2022.01.31", "2022.02.01", "2022.02.02", "2022.02.03", "2022.02.04"];
-
-
-}
