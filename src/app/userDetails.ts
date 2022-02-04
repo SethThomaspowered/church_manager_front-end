244e1c218ff07d9e4acaf9544e7319d2358adc13
Nate Allen <nateonmission@gmail.com>
2022-02-03 22:07:06 -0500

    add Church config files

---
+++ src/app/userDetails.ts
@@ -0,0 +1,9 @@
+export class User {
+
+    first: string = "Jerome";
+    last: string = "Allen";
+    accessLevel: string = "Developer";
+    loginStamps: any = ["2022.01.31", "2022.02.01", "2022.02.02", "2022.02.03", "2022.02.04"];
+
+
+}
